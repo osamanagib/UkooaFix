5283ab1e8fed13e444c1cc5cfa508a2fb9129ed3
Osama Nagib <nagioi0a@gmail.com>
2016-06-26 17:45:42 -0500

    dd plot

--- UkooaFix.pro
+++ UkooaFix.pro
@@ -1,12 +1,21 @@
 QT += core
-QT -= gui
+QT += gui
+QT       +=  widgets
 
 CONFIG += c++11
 
 TARGET = UkooaFix
 CONFIG += console
 CONFIG -= app_bundle
+QMAKE_CXXFLAGS_RELEASE = -std=c++14  -unroll-aggressive -g  -qopt-assume-safe-padding  -xCORE-AVX2 -ip -fma  -ftz -fp-model fast=2  -qopt-report-phase=vec  -O3  -falign-functions=16 -ansi-alias -fstrict-aliasing -D_REENTRANT  -fno-jump-tables
+
+INCLUDEPATH +=   /Users/nagioi0a/Downloads/qwt-6.1.2/src /opt/intel/compilers_and_libraries_2016/mac/include
+INCLUDEPATH += /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.11.sdk/System/Library/Frameworks/OpenGL.framework/Headers
+INCLUDEPATH +=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.11.sdk/System/Library/Frameworks/AGL.framework/Headers/
 
 TEMPLATE = app
 
 SOURCES += main.cpp
+LIBS += -F/usr/local/qwt-6.1.2/lib -framework qwt
+INCLUDEPATH += $$PWD/../Downloads/qwt-6.1.2/lib/qwt.framework/Versions/6/Headers
+DEPENDPATH += $$PWD/../Downloads/qwt-6.1.2/lib/qwt.framework/Versions/6/Headers
--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
-#include <QCoreApplication>
+#include <QApplication>
 #include <QObject>
 #include <QtCore>
+#include <QtGui>
+#include <QFileDialog>
+#include <qwt_matrix_raster_data.h>
+#include <qwt_color_map.h>
+#include <qwt_plot_layout.h>
+#include <qwt_plot_canvas.h>
+#include <qwt_plot_grid.h>
+#include <qwt_plot_curve.h>
+#include <qwt_plot_marker.h>
+#include <qwt_symbol.h>
+#include <qwt_scale_widget.h>
+#include <qwt_wheel.h>
+#include <qwt_plot_zoomer.h>
+#include <qwt_plot_panner.h>
+#include <qwt_plot_renderer.h>
+#include <qwt_picker_machine.h>
+#include <QPushButton>
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -24,69 +41,197 @@ signals:
 };
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
 
-    QFile *ukFile = new QFile(QString("/Users/nagioi0a/Downloads/NS1763P1004.p190"));
-    QFile *ukOut = new QFile("/Users/nagioi0a/Downloads/out.p190");
-    bool rt = ukFile->open(QIODevice::ReadOnly);
-    ukOut->open(QIODevice::WriteOnly);
-    cout << "return " << rt << endl;
-    //  char *data = new char[8200];
-    //ukFile->read(data,10);
-    qint64 icount = 0;
-    //    while (!ukFile->atEnd())
-    //    {
-    //        if(!(icount%100))
-    //        {
-    //            cout << icount << endl;
-    //            ukFile->read(data,8*(icount/100+1)+80);
-    //        }
-    qint64 fileSize = ukFile->size();
-    char *data = new char[fileSize];
-    ukFile->read(data,fileSize);
-    QString sData = data;
-    QString shot = "SNS";
-    while (true)
+    //  QGuiApplication a(argc, argv);
+    QApplication a(argc, argv);
+    QwtPlot *plot = new QwtPlot();
+    QwtPlotCanvas *canvas = new QwtPlotCanvas();
+    canvas->setBorderRadius(10);
+
+    plot->setCanvas(canvas);
+    plot->setCanvasBackground(QColor("LIGHTGRAY"));
+
+    plot->enableAxis(QwtPlot::yRight);
+    plot->enableAxis(QwtPlot::xTop);
+    plot->setAxisTitle(QwtPlot::xBottom, "Xline");
+    plot->setAxisTitle(QwtPlot::xTop, "Xline");
+    plot->setAxisTitle(QwtPlot::yLeft, "Inline");
+    plot->setAxisTitle(QwtPlot::yRight, "Inline");
+    //    float minx = srv->getStations().first().x();
+    //    float maxx = srv->getStations().last().x();
+        plot->setAxisScale( QwtPlot::xBottom,3500,300);
+    //    plot->setAxisScale( QwtPlot::xTop,minx,maxx );
+    //    QwtScaleDraw *sd = axisScaleDraw( QwtPlot::yLeft );
+    //    sd->setMinimumExtent( sd->extent( axisWidget( QwtPlot::yLeft )->font() ) );
+    plot->plotLayout()->setAlignCanvasToScales( true );
+    QFileDialog custDialog;
+    QStringList names = custDialog.getOpenFileNames(NULL, ("Open Files..."),QString(), ("UKOOA Files (*.p190 *.p90);;All Files (*)"));
+
+    // handle if the dialog was "Cancelled"
+    if(names.isEmpty())
     {
-        ukFile->seek(icount);
-        ukFile->read(data,fileSize);
-        sData = data;;
-        if(icount>fileSize)
-        {
-            break;
-        }
-        auto sPos = sData.indexOf(shot,0,Qt::CaseInsensitive);
-        QString cr = sData.mid(sPos,20);
-        if(cr.contains("\n"))
-        {
-            sPos +=2;
-        }
-        // auto shotNo  = sData.mid(sPos+20,sPos+5);
-        auto shotNos = sData.mid(sPos+20,5).toInt();
-        sPos +=27;
-        cr = sData.mid(sPos,20);
-        if(cr.contains("\n"))
-        {
-            sPos +=2;
-        }
-        auto shotx = sData.mid(sPos,8).toFloat();
-        icount +=sPos+1;
-        //     cout << shotNos << endl;
-        cout << " shot " << shotNos << " " << shotx << " "  << shotNos << endl;
+        return 0;
     }
-    //        QString oData(data);
-    //        int idx = oData.indexOf('\n');
+    qSort(names.begin(), names.end());
+    QVector <QwtPlotCurve *> curves;
+    foreach (QString name, names)
+    {
 
-    //       QString ooData = oData.mid(idx+1,80);
-    //       QString oooData = oData.mid(0,idx-1);
-    //       oooData.append(ooData);
-    //        cout << oData.toStdString()  <<endl;
-    //       // ukOut->write(oooData.toLatin1().data(),80);
-    //      //  ukOut->write("\n",1);
-    //        icount++;
-    // //   }
-    //    ukOut->flush();
-    //    ukOut->close();
+
+        QwtPlotCurve *vCurve = new QwtPlotCurve;
+       if(name.contains(QString( "NS1763")) || name.contains(QString("NS2029")))
+        {
+           QColor c = Qt::red;
+            vCurve->setSymbol( new QwtSymbol( QwtSymbol::Ellipse, Qt::red,c , QSize( 2, 2 ) ) );
+        }
+        else
+        {
+           QColor c = Qt::green;
+            vCurve->setSymbol( new QwtSymbol( QwtSymbol::Ellipse, Qt::green,c , QSize( 2, 2 ) ) );
+        }
+        vCurve->setStyle( QwtPlotCurve::NoCurve );
+        vCurve->setPen( Qt::gray );
+
+        cout << name.toStdString() << endl;
+        QVector<QPointF> curveData;
+
+        //  m_nameLineLable->setText(m_names.at(0));
+        QFile *ukFile = new QFile(QString(name));
+
+        bool rt = ukFile->open(QIODevice::ReadOnly);
+
+        cout << "return " << rt << endl;
+
+        qint64 icount = 0;
+
+        qint64 fileSize = ukFile->size();
+        char *data = new char[fileSize];
+        ukFile->read(data,fileSize);
+        QString sData = data;
+        QString shot = "SNS";
+        while (true)
+        {
+            ukFile->seek(icount);
+            ukFile->read(data,fileSize);
+            sData = data;;
+            if(icount>=fileSize)
+            {
+                break;
+            }
+            auto sPos = sData.indexOf(shot,0,Qt::CaseInsensitive);
+            QString cr = sData.mid(sPos,20);
+            if(cr.contains("\n"))
+            {
+                sPos +=2;
+            }
+            // auto shotNo  = sData.mid(sPos+20,sPos+5);
+            QString shotNo = sData.mid(sPos+20,5);
+            int shotNos;
+
+            if(shotNo.contains("\n") || shotNo.contains("\r"))
+            {
+                shotNo = sData.mid(sPos+20,7);
+                int shift1 = shotNo.indexOf("\r");
+                int shift = shotNo.indexOf("\n");
+                //    cout << shift1 << " " << shift << endl;
+                QString tmp = shotNo.mid(0,shift1);
+                tmp.append(shotNo.mid(shift+1,2));
+                shotNos = tmp.toInt();
+            }
+            else
+            {
+                shotNos = sData.mid(sPos+20,5).toInt();
+            }
+
+            float shotYs;
+            sPos = sData.indexOf(shot,0,Qt::CaseInsensitive);
+            cr = sData.mid(sPos,56);
+            if(cr.contains("\n"))
+            {
+                //       cout << " cr " << sPos << endl;
+                sPos +=2;
+            }
+
+            QString shotY = sData.mid(sPos+56,9);
+            //   cout << "shotx " << shotX.toStdString() << endl;
+            if(shotY.contains("\n") || shotY.contains("\r"))
+            {
+                shotY = sData.mid(sPos+56,11);
+                int shift1 = shotY.indexOf("\r");
+                int shift = shotY.indexOf("\n");
+                //           cout << shift1 << " " << shift << endl;
+                QString tmp = shotY.mid(0,shift1);
+                tmp.append(shotY.mid(shift+1,11));
+                shotYs = tmp.toFloat();
+            }
+            else
+            {
+                shotYs = shotY.toFloat();
+            }
+
+            float shotXs;
+            sPos = sData.indexOf(shot,0,Qt::CaseInsensitive);
+            cr = sData.mid(sPos,47);
+            if(cr.contains("\n"))
+            {
+                //       cout << " cr " << sPos << endl;
+                sPos +=2;
+            }
+
+            QString shotX = sData.mid(sPos+47,9);
+            //   cout << "shotx " << shotX.toStdString() << endl;
+            if(shotX.contains("\n") || shotX.contains("\r"))
+            {
+                shotX = sData.mid(sPos+47,11);
+                int shift1 = shotX.indexOf("\r");
+                int shift = shotX.indexOf("\n");
+                //           cout << shift1 << " " << shift << endl;
+                QString tmp = shotX.mid(0,shift1);
+                tmp.append(shotX.mid(shift+1,11));
+                shotXs = tmp.toFloat();
+            }
+            else
+            {
+                shotXs = shotX.toFloat();
+            }
+            icount +=sPos+1;
+            //     cout << shotNos << endl;
+            float shotXt = shotXs - 757551.46;
+
+
+            float shotYt = shotYs - 978769.0;
+            float shotYr = shotYt * cosf(13.661f * M_PI/180.0f) + shotXt * sinf(13.661f * M_PI/180.0f);
+            int shotYy = 981 + shotYr/25.0;
+            float shotXr = shotXt * cosf(13.661f * M_PI/180.0f) - shotYt * sinf(13.661f * M_PI/180.0f);
+            int shotXx = 2570 - shotXr/25.0;
+            if(shotXx>0 && shotYy>0)
+            {
+                QPointF shotPoint(shotXx,shotYy);
+                curveData.append(shotPoint);
+              //  cout << " shot " << shotNos << " " << shotXs  << " " << shotYs << " "  << shotXx << " " << shotYy << endl;
+            }
+
+
+
+        }
+        vCurve->setSamples(curveData);
+        curves.append(vCurve);
+        ukFile->close();
+    }
+
+
+
+    foreach (QwtPlotCurve *vCurve, curves)
+    {
+
+        vCurve->attach(plot);
+    }
+
+    //vCurve->setSamples();
+
+    plot->replot();
+    plot->show();
+
     return a.exec();
 
 }
